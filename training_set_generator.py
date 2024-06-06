0a4661225a65d22a8727f0c4fe4365767e2c336f
sam <51410648+magestee@users.noreply.github.com>
2024-06-06 11:50:22 +0330

    updated the function to generate the needed amount of training data.

--- training_set_generator.py
+++ training_set_generator.py
@@ -1,10 +1,10 @@
 import csv
 
-def generate_training_data():
-    data = [(x, x**2) for x in range(1,6)]
+def generate_training_data(rows):
+    data = [(x, x**2) for x in range(1,rows)]
     with open('training_data.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['input', 'output'])
         writer.writerow(data)
 
-generate_training_data()
+generate_training_data(100)
