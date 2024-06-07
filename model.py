548e7544604736aea9ea58e851b555138c91be0c
sam <51410648+magestee@users.noreply.github.com>
2024-06-07 16:47:21 +0330

    the weight matrices and bias vector added, leaky relue choosen for the activation function.

--- model.py
+++ model.py
@@ -1,5 +1,22 @@
 import pandas as pd
+import numpy as np
 
-data = pd.read_csv('training_data.csv')
+data = pd.read_csv('training_set.csv')
 inputs = data['input'].values
 outputs = data['output'].values
+
+np.random.seed(1)
+
+weights_i_h1 = np.random.randn(1,10)
+weights_h1_h2 = np.random.randn(10, 10)
+weights_h2_o = np.random.randn(10, 1)
+
+bias_h1 = np.zeros(10)
+bias_h2 = np.zeros(10)
+bias_o = np.zeros(1)
+
+def leaky_relu(a, alpha = 0.1):
+    if a > 0:
+        return a
+    else:
+        return alpha * a
--- training_set_generator.py
+++ training_set_generator.py
@@ -2,7 +2,7 @@ import csv
 
 def generate_training_data(rows):
     data = [(x, x**2) for x in range(1,rows)]
-    with open('training_data.csv', 'w', newline='') as csvfile:
+    with open('training_set.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['input', 'output'])
         writer.writerow(data)
