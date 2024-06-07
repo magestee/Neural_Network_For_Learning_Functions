cc36e9b67fbd727d7e74d8ea50eb270e82bec41d
sam <51410648+magestee@users.noreply.github.com>
2024-06-07 22:18:11 +0330

    calculations added for the backpropagation process but it's not completed, I should add make it functional.

--- model.py
+++ model.py
@@ -43,4 +43,32 @@ def mse(y, y_hat):
 
 output = feedforward(inputs)
 
+# calculate the loss gradient at the output
+dL_dy_hat = 2 * (output - outputs) / len(outputs)
+
+# Backpropagate through the output layer:
+dL_dz_o = dL_dy_hat # for linear activation
+dL_dW_h2_o = np.dot(a2.T, dL_dz_o)
+dL_db_o = np.sum(dL_dz_o, axis=0)
+
+# Backpropagate through the second hidden layer:
+dL_da2 = np.dot(dL_dz_o, weights_h2_o.T)
+dL_dz2 = dL_da2 * (z2 > 0) + alpha * (z2 <= 0)
+dL_dw_h1_h2 = np.dot(a1.T, dL_dz2)
+dL_db_h2 = np.sum(dL_dz2, axis=0)
+
+# Backpropagate through the first hidden layer
+dL_da1 = np.dot(dL_dz2, weights_h1_h2.T)
+dl_dz1 = dL_da1 * (z1 > 0) + alpha * (z1 <= 0)
+dL_dW_i_h1 = np.dot(inputs.T, dL_dz1)
+dL_db_h1 = np.sum(dL_dz1, axis=0)
+
+# update weights and biases
+weights_h2_o -= learning_rate * dL_dW_h2_o
+bias_o -= learning_rate * dL_db_o
+weights_h1_h2 -= learning_rate * dL_dw_h1_h2
+bias_h2 -= learning_rate * dL_db_h2
+weights_i_h1 -= learning_rate * dL_dW_i_h1
+bias_h1 -= learning_rate * dL_db_h1
+
 print(mse(output, outputs))
